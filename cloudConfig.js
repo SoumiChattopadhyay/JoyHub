222f976bc11d5d4a5f592f87b328b21f9c23b806
SoumiChattopadhyay <soumichattopadhyay09@gmail.com>
2026-06-21 09:56:43 +0530

    Changed Your Account page ui

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,5 +1,5 @@
 const cloudinary=require('cloudinary').v2;
-const CloudinaryStorage=require('multer-storage-cloudinary');
+const {CloudinaryStorage}=require('multer-storage-cloudinary');
 
 //configuring i.e. accessing cloudinary credentials in this js file
 cloudinary.config({
